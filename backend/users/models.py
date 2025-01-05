dc4787a0e58ac0994cbdee7c017bb422b315c80f
Rashib Khanal <contact@rashib.dev>
2025-01-04 22:57:06 -0500

    add: added a user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    full_name = models.CharField(max_length=500)
+    email = models.CharField(max_length=100, unique=True)
+
+        
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "full_name"]
+
+    def __str__(self):
+        return self.username
+
+
+
